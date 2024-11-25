167f978ceae802c4ff039d49fbef0cdd130fc6f9
evac2742 <evac57@nycstudents.net>
2024-11-25 15:31:19 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let ownerName = " Eva C "; // update the part BETWEEN the "quotes"
+let userName = " evac2742 "; // same here
+
+document.querySelectorAll(".owner-name").forEach((e) => {
+    e.innerHTML = ownerName;
+});
+
+document.querySelector("#github").href = "http://github.com/" + userName;
+document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
